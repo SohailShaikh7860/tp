8f53b56780439fb32a72586c2b436c43abaf391a
SohailShaikh7860 <sohailshaikh6417@gmail.com>
2025-08-01 12:38:36 +0530

    Added Divisionn Section

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ function mul(a,b){
     return a*b;
 }
 
+function div(a,b){
+    return a/b;
+}
+
 console.log("git diff...");
 
 console.log(add(1,2));
 console.log(sub(1,2));
 console.log(mul(2,22));
+console.log(dev(2,22));
